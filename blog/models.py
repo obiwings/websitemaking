bc3297b5feea199fec73d30eac265607f3ea690b
obi-hellin <chdo999@naver.com>
2022-03-31 12:55:47 +0900

    이미지 넣기

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model) :
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
 
     def __str__(self) :
         return f'{self.pk}. {(self.title)}'
