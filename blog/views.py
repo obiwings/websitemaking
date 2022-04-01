e80d455f056ec01cadec0421f3304c3940f3d9c0
obi-hellin <chdo999@naver.com>
2022-04-01 14:02:48 +0900

    태그 수정 기능이 추가

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from dataclasses import field
 import imp
 from inspect import CORO_RUNNING
 from re import U, template
+import re
+from urllib import response
 from django.shortcuts import render, redirect
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 # Create your views here.
 
@@ -29,7 +32,7 @@ class PostDetail(DetailView) :
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView) :
     model = Post
     template_name = 'page/post_form.html'
-    fields = [ 'title', 'hook_text', 'content', 'head_image', 'file_upload', 'category' ]
+    fields = [ 'title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', ]
 
     def test_func(self) :
         return self.request.user.is_superuser or self.request.user.is_staff
@@ -46,13 +49,41 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView) :
 class PostUpdate(LoginRequiredMixin, UpdateView) :
     model = Post
     template_name = 'page/post_update_form.html'
-    fields = [ 'title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags' ]
+    fields = [ 'title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', ]
+
+    def get_context_data(self, **kwargs) :
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists() :
+            tags_str_list = list()
+            for t in self.object.tags.all() :
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list)
+        return context
 
     def dispatch(self, request, *args, **kwargs) :
         if request.user.is_authenticated and request.user == self.get_object().author :
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else :
             raise PermissionDenied
+
+    def form_valid(self, form) :
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str :
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',',';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list :
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created :
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+        return response
 
 
 # def portfolio(request) :
